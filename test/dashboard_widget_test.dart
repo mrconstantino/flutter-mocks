dc53efc5cdc032bdcf8269c0d9de18d5e168d391
Marcio <mrconstantino@gmail.com>
2021-11-21 17:47:00 -0300

    Utilizando pradicates.
    Utilizando pradicates.

--- test/dashboard_widget_test.dart
+++ test/dashboard_widget_test.dart
@@ -8,10 +8,47 @@ void main() {
     await tester.pumpWidget(MaterialApp(home: Dashboard()));
     final mainImage = find.byType(Image);
     expect(mainImage, findsOneWidget);
-      });
-  testWidgets('Should display the first feature when the Dashboard is opened', (tester) async{
-  await tester.pumpWidget(MaterialApp(home: Dashboard()));
-  final firstFeature = find.byType(FeatureItem);
-  expect(firstFeature, findsWidgets);
   });
+
+  testWidgets(
+      'Should display the transfer feature when the Dashboard is opened',
+      (tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    // final iconTransferFeatureItem = find.widgetWithIcon(FeatureItem, Icons.monetization_on);
+    // expect(iconTransferFeatureItem, findsOneWidget);
+    // final nameTransferFeatureItem = find.widgetWithText(FeatureItem, 'Transfer');
+    // expect(nameTransferFeatureItem, findsOneWidget);
+
+    final transferFeatureItem = find.byWidgetPredicate((widget) =>
+        featureItemMatcher(widget, 'Transfer', Icons.monetization_on));
+    expect(transferFeatureItem, findsOneWidget);
+  });
+
+  testWidgets(
+      'Should display the transaction feed feature when the Dashboard is opened',
+      (tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    final transactionFeedFeatureItem = find.byWidgetPredicate((widget) =>
+        featureItemMatcher(widget, 'Transaction Feed', Icons.description));
+    expect(transactionFeedFeatureItem, findsOneWidget);
+    // final iconTransactionFeedFeatureItem =
+    //     find.widgetWithIcon(FeatureItem, Icons.description);
+    // expect(iconTransactionFeedFeatureItem, findsOneWidget);
+    // final nameTransactionFeedFeatureItem =
+    //     find.widgetWithText(FeatureItem, 'Transaction Feed');
+    // expect(nameTransactionFeedFeatureItem, findsOneWidget);
+  });
+
+  // testWidgets('Should display the first feature when the Dashboard is opened', (tester) async{
+  // await tester.pumpWidget(MaterialApp(home: Dashboard()));
+  // final firstFeature = find.byType(FeatureItem);
+  // expect(firstFeature, findsWidgets);
+  // });
+}
+
+bool featureItemMatcher(Widget widget, String name, IconData icon) {
+  if (widget is FeatureItem) {
+    return widget.name == name && widget.icon == icon;
+  }
+  return false;
 }
